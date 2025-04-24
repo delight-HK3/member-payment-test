65ec18bcb5c8771bdd222354d3a67e37d338e3a3
leebean <dabin310153@naver.com>
2025-04-24 19:39:18 +0900

    회원 상세조회 메서드 추가

--- src/main/java/com/example/payment/service/member/MemberService.java
+++ src/main/java/com/example/payment/service/member/MemberService.java
@@ -3,12 +3,17 @@ package com.example.payment.service.member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.payment.Enum.ResponseFailCode;
+import com.example.payment.domain.Member;
+import com.example.payment.exception.ConflictException;
 import com.example.payment.exception.NoSearchException;
 import com.example.payment.model.member.MemberRequestDTO;
 import com.example.payment.model.member.MemberResponseDTO;
 import com.example.payment.repository.member.MemberRepository;
+
+import jakarta.persistence.OptimisticLockException;
 
 @Service
 public class MemberService {
@@ -24,7 +29,7 @@ public class MemberService {
      * 
      * @param sort
      * @param pageable
-     * @return
+     * @return Page<MemberResponseDTO>
      */
     public Page<MemberResponseDTO> getMemberList(MemberRequestDTO memberRequestDTO, Pageable pageable){
         
@@ -36,4 +41,22 @@ public class MemberService {
         return memberResult;
     }   
 
+    /**
+     * 특정 회원 조회 (조회 성공시 조회수 1증가)
+     * 
+     * @param memberid
+     * @return Member
+     */
+    @Transactional
+    public Member getMemberDetail(Long memberid){
+        Member member = memberRepository.findById(memberid)
+                            .orElseThrow(() -> new NoSearchException(ResponseFailCode.NO_SEARCH_MEMBER));
+        try{
+            member.plusViewCount();
+            memberRepository.save(member);
+            return member;
+        } catch (OptimisticLockException e){ // 낙관적 잠금 충돌감지
+            throw new ConflictException(ResponseFailCode.DATABASE_CONFLICT);
+        }
+    }
 }
