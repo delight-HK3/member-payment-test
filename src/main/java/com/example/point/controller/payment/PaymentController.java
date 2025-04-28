83b96a4a0a057a4ea62d515610581114981b2639
leebean <dabin310153@naver.com>
2025-04-28 18:50:19 +0900

    Update PaymentController.java

--- src/main/java/com/example/point/controller/payment/PaymentController.java
+++ src/main/java/com/example/point/controller/payment/PaymentController.java
@@ -1,8 +1,74 @@
 package com.example.point.controller.payment;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.point.model.api.ApiResponse;
+import com.example.point.model.payment.PaymentRequestDTO;
+
+import jakarta.validation.Valid;
+
 @RestController
+@RequestMapping("/point/v1")
 public class PaymentController {
     
+    @Value("${toss.api.key.test}")
+    private String tossApiKey; // toss API key
+
+    @GetMapping("/payment")
+    public ResponseEntity<JSONObject> confirmPayment(@Valid PaymentRequestDTO paymentRequestDTO) throws Exception {
+
+        String orderid = "chdimFOf9tnXV5u8Xqtlodgsdf";
+
+        JSONParser parser = new JSONParser();
+
+        JSONObject obj = new JSONObject();
+        obj.put("orderId", orderid);
+        obj.put("amount", paymentRequestDTO.getAmount());
+        obj.put("paymentKey", tossApiKey);
+
+        // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
+        // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
+        // @docs https://docs.tosspayments.com/reference/using-api/authorization#%EC%9D%B8%EC%A6%9D
+        Base64.Encoder encoder = Base64.getEncoder();
+        byte[] encodedBytes = encoder.encode((tossApiKey + ":").getBytes(StandardCharsets.UTF_8));
+        String authorizations = "Basic " + new String(encodedBytes);
+
+        URL url = new URL("https://api.tosspayments.com/v1/payments/confirm");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Authorization", authorizations);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+            
+        OutputStream outputStream = connection.getOutputStream();
+        outputStream.write(obj.toString().getBytes("UTF-8"));
+
+        int code = connection.getResponseCode();
+        boolean isSuccess = code == 200;
+
+        InputStream responseStream = isSuccess ? connection.getInputStream() : connection.getErrorStream();
+        
+        Reader reader = new InputStreamReader(responseStream, StandardCharsets.UTF_8);
+        JSONObject jsonObject = (JSONObject) parser.parse(reader);
+        responseStream.close();
+
+        return ResponseEntity.status(code).body(jsonObject);
+    }
+
 }
